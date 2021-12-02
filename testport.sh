7df0eebba540dfeb8ded5d547b96e0a999833cc1
Zahar Mirakin <zaxargaubica@gmail.com>
2021-12-02 15:23:11 +0300

    Create testport.sh

---
+++ testport.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if grep -qvP "^$" <<< $1;
+
+then
+
+    adapter_name=${1};
+
+
+	if grep -qP "^\d+$" <<< $2;
+
+	then
+
+		i=${2};
+
+	else
+		echo "Use test1.sh --help";
+
+		exit 1;
+	fi
+
+
+elif grep -qP -- "--help" <<< $2;
+
+then
+
+	echo "sudo test1.sh ADAPTER_NAME IFACE_NUM" ;
+
+	exit 0;
+
+else
+
+	echo "Use test1.sh --help for usage info";
+
+	exit 1;
+
+fi
+
+	ifconfig $adapter_name 192.168.${i}.100;
+
+	route add default gw 192.168.$i.1;
+
+	ping 192.168.${i}.100 -c 3;
+
+	ifconfig | grep -e "inet";
+
+	ethtool $adapter_name | grep -e "Speed": -e "Duplex";
+
+
