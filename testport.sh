8bf5d2ebc487af1873c567a2cd1d8fc59e80bedf
Zahar Mirakin <zaxargaubica@gmail.com>
2021-12-28 18:06:42 +0300

    Update testport.sh

--- testport.sh
+++ testport.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
-
+#Displaying information about the operating modes of a physical network port
 if grep -qvP "^$" <<< $1;
 
-then
+  then
 
     adapter_name=${1};
 
+if grep -qP "^\d+$" <<< $2;
 
-	if grep -qP "^\d+$" <<< $2;
+  then
+      
+    i=${2};
 
-	then
+  else
+      
+    echo "Use test1.sh --help";
 
-		i=${2};
-
-	else
-		echo "Use test1.sh --help";
-
-		exit 1;
-	fi
-
+  exit 1;
+  
+fi
 
 elif grep -qP -- "--help" <<< $2;
 
-then
+  then
 
-	echo "sudo test1.sh ADAPTER_NAME IFACE_NUM" ;
-
-	exit 0;
+    echo "sudo test1.sh ADAPTER_NAME IFACE_NUM" ;
+  
+  exit 0;
 
 else
 
-	echo "Use test1.sh --help for usage info";
-
-	exit 1;
+  echo "Use test1.sh --help for usage info";
+ 
+exit 1;
 
 fi
 
